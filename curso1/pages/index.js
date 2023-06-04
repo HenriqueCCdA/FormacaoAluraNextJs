4c3ad70b4689c75bed70d4665c76802838ad17c9
Henrique Andrade <henrique.ccda@gmail.com>
2023-06-04 13:59:13 -0300

    Curso 1 - Aula 3.3

--- curso1/pages/index.js
+++ curso1/pages/index.js
@@ -1,33 +1,40 @@
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screen/HomeScreen";
+
+export default HomeScreen;
 
 
 
-
-function Title({children, as}) {
-
-    const Tag = as;
-    return (
-        <>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            `}</style>
-        </>
-    )
-}
+// import Link from "../src/components/Link";
+// import Footer from "../src/components/patterns/Footer";
 
 
-export default function HomePage() {
-    return (
-        <div>
-            <Title as="h2">Alura Cases - Home Page</Title>
-            <Link href="/faq">
-                Ir para o FAQ
-            </Link>
-        </div>
-    )
-}
+
+// function Title({children, as}) {
+
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </>
+//     )
+// }
+
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as="h2">Alura Cases - Home Page</Title>
+//             <Link href="/faq">
+//                 Ir para o FAQ
+//             </Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
