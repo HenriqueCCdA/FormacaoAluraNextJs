9c9486e2a1f9a475a82f56ea020d3d299490b0df
Henrique Andrade <henrique.ccda@gmail.com>
2023-06-08 19:30:00 -0300

    Publicando na vercel.

--- curso1/pages/faq.js
+++ curso1/pages/faq.js
@@ -1,7 +1,8 @@
-import Head from "next/head";
-import Link from "../src/components/Link";
+import FAQScreen from "../src/screen/FAQScreen";
 
-export  async function getStaticProps(context) {
+export  default FAQScreen;
+
+export async function getStaticProps(context) {
     console.log("\nRodando no build\n")
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
     const faq = await fetch(FAQ_API_URL)
@@ -14,32 +15,8 @@ export  async function getStaticProps(context) {
 
     return {
         props: {
+            qualquercoisa: 'que eu passar aqui',
             faq
         },
     };
-}
-
-
-export default function FAQPage({faq}) {
-    return (
-        <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
-            <h1>Alura Casas - Página de perguntas FAQ</h1>
-            <Link href="/">
-                Ir para a home
-            </Link>
-            <ul>
-                {faq.map(({ answer, question })=> (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
 }
